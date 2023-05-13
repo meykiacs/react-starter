4e127dac7af99ff155d837325ce904cb5db7c6c7
Meysam Kia <meykiacs@gmail.com>
2023-05-13 16:06:28 +0330

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { createRoot } from "react-dom/client"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Details from "./Details"
+import Root from "./Route"
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hey</h1>
-    </div>
-  )
-}
+const App = () => (
+  <BrowserRouter>
+    <h1>App Title</h1>
+    <Link to="/">Root</Link><br />
+    <Link to="/details/1">detail1</Link>
+    <Routes>
+      <Route path="/details/:id" element={<Details />} />
+      <Route path="/" element={<Root />} />
+    </Routes>
+  </BrowserRouter>
+)
 
 const container = document.getElementById("root")
 const root = createRoot(container)
